7d248e28c4413747aa542ae6d38e550dda9701ab
mesb <marcus.brj@gmail.com>
2025-03-30 20:25:07 -0300

    starting widgets composition (ipfix textview widget)

--- main.go
+++ main.go
@@ -145,13 +145,16 @@ func main() {
 	startFileCollector("wireshark_new.ipfix", "cert_ipfix.xml")
 	defer C.freeMemory()
 
-	all_recs_as_txt := getAllRecords()
-
-	var app = tview.NewApplication()
-	var text = tview.NewTextView().
-		SetTextColor(tcell.ColorGreen).
-		SetText(all_recs_as_txt)
-	if err := app.SetRoot(text, true).EnableMouse(true).Run(); err != nil {
+	app := tview.NewApplication()
+	flex := tview.NewFlex().
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("Left (1/2 x width of Top)"), 0, 1, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(tview.NewBox().SetBorder(true).SetTitle("Top"), 0, 1, false).
+			AddItem(tview.NewTextView().SetLabel("   IPFIX Records:   ").
+				SetTextColor(tcell.ColorGreen).SetText(getAllRecords()), 0, 3, false).
+			AddItem(tview.NewBox().SetBorder(true).SetTitle("Bottom (5 rows)"), 5, 1, false), 0, 2, false).
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("Right (20 cols)"), 20, 1, false)
+	if err := app.SetRoot(flex, true).EnableMouse(true).SetFocus(flex).Run(); err != nil {
 		panic(err)
 	}
 
